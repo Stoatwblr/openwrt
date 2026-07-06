17ed855bc122f71ec57282d0e94b49ca6ba2efb0
Alan Brown <alan@Riffraff.fritz.box>
2026-07-06 13:04:41 +0100

    20260706-2

--- target/linux/econet/en751627/target.mk
+++ target/linux/econet/en751627/target.mk
@@ -7,7 +7,8 @@ CPU_TYPE:=1004kc
 KERNELNAME:=vmlinuz.bin
 FEATURES:=pci page_pool
 
-DEFAULT_PACKAGES += kmod-mtk-eth-soc -kmod-econet-eth kmod-leds-gpio kmod-gpio-button-hotplug wpad-mbedtls 
+# Bind mainline Airoha Ethernet framework and the native MT7530 DSA switch driver modules
+DEFAULT_PACKAGES += kmod-airoha-eth -kmod-econet-eth kmod-switch-mt7530 kmod-leds-gpio kmod-gpio-button-hotplug wpad-mbedtls 
 
 define Target/Description
 	Build firmware images for EcoNet EN7516 and EN7527 based boards.
