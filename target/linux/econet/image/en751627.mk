29015393c63c07a450e2b2b96bd8c01e54cad6e5
Alan Brown <alan@Riffraff.fritz.box>
2026-07-07 23:58:41 +0100

    2026070723

--- target/linux/econet/image/en751627.mk
+++ target/linux/econet/image/en751627.mk
@@ -218,7 +218,5 @@ define Image/Prepare
 	# 1. Fetch and stage open-source wireless firmware assets from kernel.org
 	$(STAGING_DIR_HOST)/bin/bash ./sync_upstream_firmware.sh $(TARGET_DIR)
 
-	# 2. Extract and inject generic MediaTek proprietary DSL/VoIP modules
-	$(STAGING_DIR_HOST)/bin/bash ./extract_en751627_zyxel_blobs.sh $(TARGET_DIR)
 endef
 
